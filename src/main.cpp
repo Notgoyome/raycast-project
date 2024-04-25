83cd74f52eff85729483f714947f3167a9c7cdc3
Guillame Tran <guillaume1.tran@epitech.eu>
2024-04-25 10:22:21 +0200

    ADD hello world

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-MPL-4-1-raytracer-robin.glaude
+** File description:
+** main.cpp
+*/
+
+#include "../include/main.hpp"
+
+int main(int ac, char **av)
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
