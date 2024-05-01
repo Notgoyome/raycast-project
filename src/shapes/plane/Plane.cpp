5098368c1a9139df6483ad1712c0f61cf66b850f
Guillame Tran <guillaume1.tran@epitech.eu>
2024-05-01 21:49:00 +0200

    ADD plane (not tested, will test it late in main)

--- src/shapes/plane/Plane.cpp
+++ src/shapes/plane/Plane.cpp
@@ -4,36 +4,52 @@
 
 #include "Plane.hpp"
 
-Plane::Plane()
+ray::Plane::Plane()
 {
 
 }
 
-Maybe<Math::Point3D> Plane::hit(const ray::Ray& ray) {
-    (void)ray;
-    // Math::Point3D center = getPosition();
-    // Math::Vector3D normal = getNormale(center);
-//    Math::Vector3D d = ray.origin - center;
-//    double product = d * normal;
-//
-//    if (fabs(product) < 0.0001)
-//        return Maybe<Math::Point3D>();
-//    double t = -product / (ray.direction * normal);
-    return Maybe<Math::Point3D>{};
+Maybe<Math::Point3D> ray::Plane::hit(const ray::Ray& ray) {
+    Math::Vector3D vectorNormale = getNormale(Math::Point3D{0,0,0});
+    Math::Point3D point = getPosition();
+    double t = 0;
+    double denom = (vectorNormale.X * ray.direction.X +
+                vectorNormale.Y * ray.direction.Y +
+                vectorNormale.Z * ray.direction.Z);
+    if (denom < 1e-6) {
+        return Maybe<Math::Point3D>{};
+    }
+    t = ((point.X - ray.origin.X) * vectorNormale.X +
+            (point.Y - ray.origin.Y) * vectorNormale.Y +
+            (point.Z - ray.origin.Z) * vectorNormale.Z) / denom;
+    if (t < 0) {
+        return Maybe<Math::Point3D>{};
+    }
+    return Maybe<Math::Point3D>{ray.origin + ray.direction * t};
 }
 
-Math::Vector3D Plane::getNormale(__attribute__((unused))const Math::Point3D& point)
+Math::Vector3D ray::Plane::getNormale(__attribute__((unused))const Math::Point3D& point)
 {
-    Math::Matrix<3,1> normale;
+    Math::Matrix<4,1> normale;
     normale(0,0) = 0;
-    normale(0,1) = 1;
-    normale(0,2) = 0;
+    normale(1,0) = 1;
+    normale(2,0) = 0;
+    normale(3,0) = 0;
     Math::Vector3D vectorNormale;
-    Math::Matrix<3,3> rotation = getRotation();
 
-    normale = rotation * normale;
+    normale = _transformMatrix * normale;
     vectorNormale.X = normale(0,0);
-    vectorNormale.Y = normale(0,1);
-    vectorNormale.Z = normale(0,2);
+    vectorNormale.Y = normale(1,0);
+    vectorNormale.Z = normale(2,0);
     return vectorNormale;
+}
+
+extern "C" ray::Plane *create(std::map<std::string, std::string>& properties)
+{
+    return new ray::Plane();
+}
+
+extern "C" ray::type getType()
+{
+    return ray::type::SHAPE;
 }
--- src/shapes/plane/Plane.hpp
+++ src/shapes/plane/Plane.hpp
@@ -11,15 +11,23 @@
 #include "Ray.hpp"
 #include <cmath>
 #include <memory>
+#include <map>
 
-class Plane : public ray::AShape {
-public:
-    Plane();
-    ~Plane() override = default;
-    Maybe<Math::Point3D> hit(const ray::Ray& ray) override;
-    Math::Vector3D getNormale(const Math::Point3D& point) override;
-private:
-};
+namespace ray {
+    class Plane : public ray::AShape {
+    public:
+        Plane();
 
+        ~Plane() override = default;
+
+        Maybe<Math::Point3D> hit(const ray::Ray &ray) override;
+
+        Math::Vector3D getNormale(const Math::Point3D &point) override;
+
+    private:
+    };
+}
+extern "C" ray::Plane *create(std::map<std::string, std::string>& properties);
+extern "C" ray::type getType();
 
 #endif //B_OOP_400_MPL_4_1_RAYTRACER_ROBIN_GLAUDE_PLANE_HPP
