20d916d2c2e3d29ec4ef9834eb537dfdbd25aefa
martonroux <marton.roux@epitech.eu>
2024-05-02 23:53:00 +0200

    create ACamera so it enables the creation of future other types of cameras

--- src/cameras/base/Camera.hpp
+++ src/cameras/base/Camera.hpp
@@ -8,22 +8,27 @@
 #ifndef CAMERA_HPP
 #define CAMERA_HPP
 
+#include <map>
+
 #include "Ray.hpp"
 #include "math/Rectangle3D.hpp"
 #include "math/Point3D.hpp"
+#include "../ACamera.hpp"
 
 namespace ray {
 
-    class Camera {
+    class Camera : public ACamera {
     public:
         Math::Point3D origin{0, 0, 0};
         Math::Rectangle3D screen;
 
-        Camera(double width, double height);
+        Camera(double width, double height, double fieldOfView);
 
-        [[nodiscard]] ray::Ray ray(double u, double v) const;
+        [[nodiscard]] ray::Ray ray(double u, double v) const override;
     };
 
 } // Raytracer
+
+extern "C" ray::INode *create(const std::map<std::string, std::string> &attributes);
 
 #endif //CAMERA_HPP
