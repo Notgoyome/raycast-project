fdadf9a54d488284938d133f502bebc763a5329c
martonroux <marton.roux@epitech.eu>
2024-05-07 20:10:47 +0200

    remove Wikipedia comments

--- src/materials/perlin/PerlinMaterial.cpp
+++ src/materials/perlin/PerlinMaterial.cpp
@@ -12,58 +12,40 @@
 namespace ray {
 
     double interpolate(double a0, double a1, float w) {
-        /* // You may want clamping by inserting:
-         * if (0.0 > w) return a0;
-         * if (1.0 < w) return a1;
-         */
         return (a1 - a0) * w + a0;
-        /* // Use this cubic interpolation [[Smoothstep]] instead, for a smooth appearance:
-         * return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
-         *
-         * // Use [[Smootherstep]] for an even smoother result with a second derivative equal to zero on boundaries:
-         * return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
-         */
     }
 
     Math::Vector2D randomGradient(int ix, int iy) {
-        // No precomputed gradients mean this works for any number of grid coordinates
         const unsigned w = 8 * sizeof(unsigned);
-        const unsigned s = w / 2; // rotation width
+        const unsigned s = w / 2;
         unsigned a = ix, b = iy;
         a *= 3284157443; b ^= a << s | a >> (w-s);
         b *= 1911520717; a ^= b << s | b >> (w-s);
         a *= 2048419325;
-        double random = a * (3.14159265 / ~(~0u >> 1)); // in [0, 2*Pi]
+        double random = a * (3.14159265 / ~(~0u >> 1));
         Math::Vector2D v{0, 0};
         v.first = std::cos(random); v.second = std::sin(random);
         return v;
     }
 
     double dotGridGradient(int ix, int iy, double x, double y) {
-        // Get gradient from integer coordinates
         Math::Vector2D gradient = randomGradient(ix, iy);
 
-        // Compute the distance vector
         double dx = x - ix;
         double dy = y - iy;
 
-        // Compute the dot-product
         return (dx*gradient.first + dy*gradient.second);
     }
 
     double perlin(double x, double y) {
-        // Determine grid cell coordinates
         int x0 = static_cast<int>(std::floor(x));
         int x1 = x0 + 1;
         int y0 = static_cast<int>(std::floor(y));
         int y1 = y0 + 1;
 
-        // Determine interpolation weights
-        // Could also use higher order polynomial/s-curve here
         double sx = x - x0;
         double sy = y - y0;
 
-        // Interpolate between grid point gradients
         double n0, n1, ix0, ix1, value;
 
         n0 = dotGridGradient(x0, y0, x, y);
@@ -75,14 +57,14 @@ namespace ray {
         ix1 = interpolate(n0, n1, sx);
 
         value = interpolate(ix0, ix1, sy);
-        return value * 0.5 + 0.5; // Will return in range -1 to 1. To make it in range 0 to 1, multiply by 0.5 and add 0.5
+        return value * 0.5 + 0.5;
     }
 
     double perlinOctaves(double x, double y, int octaves, double persistence) {
         double total = 0;
         double frequency = 1;
         double amplitude = 1;
-        double maxValue = 0;  // Used for normalizing result to 0.0 - 1.0
+        double maxValue = 0;
 
         for(int i=0; i < octaves; i++) {
             total += perlin(x * frequency, y * frequency) * amplitude;
